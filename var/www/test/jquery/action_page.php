948819b84ec18bad432c62669021664458bd3011
martinforsyth <martin@forfamily.co.uk>
2018-11-20 11:52:18 +0000

    Clearly separating WebApp scripts stored in different Google accounts

--- var/www/test/jquery/action_page.php
+++ var/www/test/jquery/action_page.php
@@ -5,29 +5,11 @@ Passing date range <?php echo htmlspecialchars($_POST['datefilter']); ?> to goog
 // https://evertpot.com/curl-redirect-requestbody/
 // http://php.net/manual/en/function.curl-setopt.php
 
+// getcalendarevents project in shaftesburyuc@gmail.com scripts account
+$url = 'https://script.google.com/macros/s/AKfycbw6U9HbKGRMxYWtl1omIUdSaL4jCOInH56JBqLOwwAod4IkbP18/exec?daterange=' . urlencode(htmlspecialchars($_POST['datefilter']));
 
-// See this in browser ...
-//	Passing date range 22/11/2018 to 22/12/2018 to google scripts webapp via curl HTTP GET request.
-//	Moved Temporarily
-// The document has moved here. 1
-//
-// where here link is ...
-// https://accounts.google.com/ServiceLogin?passive=1209600&continue=https://script.google.com/macros/s/AKfycbzti4_uUaGNWAub1_HA6bhQTtMttU8wuVWl6X70oJm5sLUl8yg/exec?daterange%3D22/11/2018%2Bto%2B22/12/2018&followup=https://script.google.com/macros/s/AKfycbzti4_uUaGNWAub1_HA6bhQTtMttU8wuVWl6X70oJm5sLUl8yg/exec?daterange%3D22/11/2018%2Bto%2B22/12/2018
-//
-// and curl opts set to ...
-//
-// curl_setopt($curl, CURLOPT_URL, $url);
-// curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-// curl_setopt($curl,CURLOPT_FOLLOWLOCATION, true);
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//
-// and web app deploy cfg set to execute as anyone - NOT anyone + anonymous			
-
-
-
-
-
-$url = 'https://script.google.com/macros/s/AKfycbzti4_uUaGNWAub1_HA6bhQTtMttU8wuVWl6X70oJm5sLUl8yg/exec?daterange=' . urlencode(htmlspecialchars($_POST['datefilter']));
+// getcalendarevents project in bsucshaftesbury@gmail.com scripts account
+//$url = 'https://script.google.com/macros/s/AKfycbzti4_uUaGNWAub1_HA6bhQTtMttU8wuVWl6X70oJm5sLUl8yg/exec?daterange=' . urlencode(htmlspecialchars($_POST['datefilter']));
 
 $curl = curl_init();
 
