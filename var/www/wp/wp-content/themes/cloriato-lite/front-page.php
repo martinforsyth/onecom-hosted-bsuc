26a845a253d97667e57d148b268b57ed44803f7d
martinforsyth <martin@forfamily.co.uk>
2018-12-02 21:13:34 +0000

    original version of front page

---
+++ var/www/wp/wp-content/themes/cloriato-lite/front-page.php
@@ -0,0 +1,55 @@
+<?php
+/*
+/**
+ * The main front page file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query. 
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ */
+?>
+<?php get_header(); ?>
+<!--Start Slider Wrapper-->
+
+<br /><img src="images/interior-front-page-950.jpg" width="950" height="250" /><br />
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td style="background:#FFFFFF; border:none; padding-left:20px; padding-right:10px; vertical-align:top;"><h1>Welcome!</h1><h4><table border="0" align="left" cellpadding="0" cellspacing="0" >
+  <tr>
+    <td class="sml" style="padding-right:20px; padding-bottom:0px;"><div align="center"><img src="./images/Pentecost.jpg" /><br />
+    <em>Pentecost by Judy Selbie</em></div></td>
+  </tr>
+</table>Bell Street United Church (Methodist & United Reformed) is an active church in Shaftesbury, an historic hilltop town. We are a family church offering a warm welcome to people of all ages.</h4>
+<h4>As a town centre church we also reach out to people in the community, in need, especially with baptisms, weddings and funerals.</h4>
+<h4>These pages will give you a brief introduction to what we do. Should you wish to know more then why not get in touch with us.</h4>
+</td>
+<td align="left" valign="top" width="300px" style="background:#FFFFFF; border:none;">
+  <p class="sml"><strong>Sunday:</strong><br />
+  &bull;&nbsp;Morning Service - 10.30am<br />
+  &bull;&nbsp;Evening Service - 6.00pm</p>
+	
+  <p class="sml"><strong>Wednesday:</strong><br />
+    &bull;&nbsp;9.30 am Roundabout  Parent, Baby and Toddler Group<br />
+    &bull;&nbsp;9.30 am Children and Toddler Clothing Exchange<br />
+    &bull;&nbsp;10.30 am Bible Study
+</p>
+	
+  <p class="sml"><strong>Thursday:</strong><br />
+	&bull;&nbsp;Church open for prayer (10.00am - 2.00 pm)<br />
+	&bull;&nbsp;Sunshine Hour - 2.30 pm</p>
+	
+  <p class="sml"><strong>Friday:</strong><br />
+	&bull;&nbsp;Friday Food & Fellowship (10.00 am - 12.00 pm)<br />
+  &bull;&nbsp;Pause for Thought - 11.00 am</p> </td>
+  </tr>
+</table>
+
+<!--End home content wrapper-->
+<div class="clear"></div>
+</div>
+<!--End Container-->
+<?php get_footer(); ?>
